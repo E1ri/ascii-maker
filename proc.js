43096e0039fb97bc32581c78ae3a58d7dc32d7f0
Vladimir Firsov <hokk3dverum@gmail.com>
2021-05-09 14:44:04 +0300

    minor refactoring

--- proc.js
+++ proc.js
@@ -1,13 +1,14 @@
 const sharp = require('sharp');
 
-const input = 'lain2.jpg'; //image name (example.jpg)
+const input = 'image.png'; //image name (example.jpg)
 
+const range = ` '":;!*%#$@`.split('');
 const image = sharp(input);
 image
     .metadata()
     .then(metadata => {
         return image
-            .resize(Math.round(metadata.width / 4)) //You can change size here example: .resize(width, height) or .resize(Math.round(metadata.width/YOUR_NUMBER))
+            .resize(Math.round(metadata.width / 5)) //You can change size here example: .resize(width, height) or .resize(Math.round(metadata.width/YOUR_NUMBER))
             .gamma()
             .grayscale()
             .raw()
@@ -18,66 +19,11 @@ image
         const newPixelArray = [];
         const width = info.width;
 
-        for (let x of pixelArray) {
-            newPixelArray.push(Math.ceil(x / 255 * 10));
+        for (let x = 0; x < pixelArray.length; x++) {
+            newPixelArray.push(range[Math.round(pixelArray[x] / 255 * 10)] + ' ');
+            if (x % width === 0) {
+                newPixelArray.push('\n');
+            }
         }
-        return {
-            newPixelArray,
-            width
-        }
+        console.log(newPixelArray.join(''));
     })
-    .then((get) => {
-        const arr = get.newPixelArray;
-        const width = get.width;
-        for (let n = 0; n <= arr.length; n++) {
-            switch (arr[n]) {
-                case 0:
-                    arr[n] = '  '
-                    break;
-            
-                case 1:
-                    arr[n] = "' "
-                    break;
-
-                case 2:
-                    arr[n] = '" '
-                    break;
-
-                case 3:
-                    arr[n] = ': '
-                    break;
-
-                case 4:
-                    arr[n] = '; '
-                    break;
-                
-                case 5:
-                    arr[n] = '! '
-                    break;
-                
-                case 6:
-                    arr[n] = '* '
-                    break;
-
-                case 7:
-                    arr[n] = '# '
-                    break;
-
-                case 8:
-                    arr[n] = '$ '
-                    break;
-
-                case 9:
-                    arr[n] = '% '
-                    break;
-
-                case 10:
-                    arr[n] = '@ '
-                    break;
-            }
-            if (n % (width + 1) === 0) {
-                arr.splice(n, 0, '\n')
-            }
-        }
-        console.log(arr.join(''));
-    });
